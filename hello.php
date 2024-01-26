aca9e9b6a5e1095a04e9775158aef7b302bdc458
Kanchana Nilmini Pusweli Mulla Gamage <nilminipuswella@gmail.com>
2024-01-26 12:06:12 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+ 
+<!--This is printing hello-->
+<?php
+// this is printing hello world
+/*multiple
+line
+comment*/
+echo "<h1>Hello World</h1>";
+?>
+
+
+</body>
+</html>
+
+
+
